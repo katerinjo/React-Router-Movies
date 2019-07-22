486b0b16a899c132b69c930c5959750c0f438e0f
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-22 13:36:38 -0700

    add movie list

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList.js';
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
     </div>
   );
 };
